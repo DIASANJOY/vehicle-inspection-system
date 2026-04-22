7b9b5ed139607092a6ed6c213589db5d9b65e16a
DIASANJOY <late4377@gmail.com>
2026-04-22 08:01:21 +0700

    Implemented React Router and updated coordinate calculation logic

--- main.jsx
+++ main.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Vector from './Vector.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Vector />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        {/* Route mengatur halaman mana yang muncul berdasarkan URL */}
+        <Route path="/" element={<Vector />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
